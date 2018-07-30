73b822724eafd8b8d6a85013281d7fb48f620d76
Andrew <afuselier23@gmail.com>
2018-07-30 14:52:20 -0700

    adding boilerplate

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 // Root of application must live in src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
